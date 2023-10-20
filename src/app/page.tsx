d2c4e71f2f5bcadfe858cefa87a0e7e13a6421db
Paulo Turino <paulo.turino01@gmail.com>
2023-10-20 11:33:17 -0300

    feat: Add change handdler and validation on file sending

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [jsonFile, setJsonFile] = useState<File | undefined>(undefined);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>JSON Tree Viewer</h1>
       <h3 className={styles.subtitle}>Simple JSON Viewer that runs completely on-client. No data exchange</h3>
       <label className={styles.button} htmlFor="file" tabIndex={0}>Load JSON</label>
-      <input type='file' accept='.json' name='file' id='file' className={styles.input} />
-      <p className={styles.err}>Invalid file. Please load a valid JSON file.</p>
+      <input type='file' accept='.json' name='file' id='file' className={styles.input} onChange={(e) => e.target.files && setJsonFile(e.target.files[0])} />
+      {!jsonFile ? null : jsonFile.type !== "application/json" &&
+        <p className={styles.err}>Invalid file. Please load a valid JSON file.</p>
+      }
     </main>
   )
 }
