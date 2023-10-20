a7035d910aecdef48eaf542e55be28bc4ba10ecd
Paulo Turino <paulo.turino01@gmail.com>
2023-10-20 14:36:16 -0300

    feat: Turn file into json and render it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,21 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const [jsonFile, setJsonFile] = useState<File | undefined>(undefined);
+  const [jsonText, setJsonText] = useState<object | undefined>(undefined);
 
   if (jsonFile) {
     const reader = new FileReader();
 
     reader.onload = (event) => {
       if (event.target) {
-        const text = event.target.result;
-        console.log(text);
-      } else {
-        console.log("deu errado");
+        const text = event.target.result?.toString() as string;
+        try {
+          const jsonData = JSON.parse(text);
+          setJsonText(jsonData);
+        } catch (error) {
+          console.error("Erro ao analisar o JSON:", error);
+          setJsonText(undefined);
+        }
       }
     };
 
@@ -45,6 +50,12 @@ export default function Home() {
               Invalid file. Please load a valid JSON file.
             </p>
           )}
+          {jsonText && (
+        <div>
+          <h2>{jsonFile?.name}</h2>
+          <pre>{JSON.stringify(jsonText, null, 2)}</pre>
+        </div>
+      )}
     </main>
   );
 }
