af212c15a3e9a2645dc13657b40deda317ca59a1
Paulo Turino <paulo.turino01@gmail.com>
2023-10-20 08:35:52 -0300

    feat: Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello, World!</h1>
+      <h1 className={styles.title}>JSON Tree Viewer</h1>
+      <h3 className={styles.subtitle}>Simple JSON Viewer that runs completely on-client. No data exchange</h3>
+      <button className={styles.button}>Load JSON</button>
+      <p className={styles.err}>Invalid file. Please load a valid JSON file.</p>
     </main>
   )
 }
