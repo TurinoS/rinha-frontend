55523b74cfeb92eb89bb4d825bdd29ff16b24721
Paulo Turino <paulo.turino01@gmail.com>
2023-10-20 13:54:04 -0300

    add: Add reading file function and console.log json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,50 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import styles from './page.module.css'
+import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
   const [jsonFile, setJsonFile] = useState<File | undefined>(undefined);
 
+  if (jsonFile) {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      if (event.target) {
+        const text = event.target.result;
+        console.log(text);
+      } else {
+        console.log("deu errado");
+      }
+    };
+
+    reader.readAsText(jsonFile);
+  }
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>JSON Tree Viewer</h1>
-      <h3 className={styles.subtitle}>Simple JSON Viewer that runs completely on-client. No data exchange</h3>
-      <label className={styles.button} htmlFor="file" tabIndex={0}>Load JSON</label>
-      <input type='file' accept='.json' name='file' id='file' className={styles.input} onChange={(e) => e.target.files && setJsonFile(e.target.files[0])} />
-      {!jsonFile ? null : jsonFile.type !== "application/json" &&
-        <p className={styles.err}>Invalid file. Please load a valid JSON file.</p>
-      }
+      <h3 className={styles.subtitle}>
+        Simple JSON Viewer that runs completely on-client. No data exchange
+      </h3>
+      <label className={styles.button} htmlFor="file" tabIndex={0}>
+        Load JSON
+      </label>
+      <input
+        type="file"
+        accept=".json"
+        name="file"
+        id="file"
+        className={styles.input}
+        onChange={(e) => e.target.files && setJsonFile(e.target.files[0])}
+      />
+      {!jsonFile
+        ? null
+        : jsonFile.type !== "application/json" && (
+            <p className={styles.err}>
+              Invalid file. Please load a valid JSON file.
+            </p>
+          )}
     </main>
-  )
+  );
 }
