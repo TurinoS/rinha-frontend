7b81bc3f10639808a5509fa6a92eaaf461ab9df9
Paulo Turino <paulo.turino01@gmail.com>
2023-10-23 10:22:09 -0300

    feat: add second step on tree viewer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,22 @@ export default function Home() {
           {Object.keys(jsonText[0]).map((item, index) => (
             <div key={index}>
               <p>{item}: </p>
-              <pre>{JSON.stringify(jsonText[0][item], null, 2)}</pre>
+              {typeof jsonText[0][item] === "object" && !Array.isArray(jsonText[0][item]) ?
+                Object.keys(jsonText[0][item]).map((item, index) => (
+                  <div key={index} className={styles.treeDiv}>
+                    <p>{item}: </p>
+                  </div>
+                )) : typeof jsonText[0][item] === "object" && Array.isArray(jsonText[0][item]) ?
+                jsonText[0][item].map((item: any, index: number) => (
+                  <div key={index} className={styles.treeDiv}>
+                    <p>{item}: </p>
+                  </div>
+              )): <p className={styles.treeDiv}>{jsonText[0][item] ? jsonText[0][item] : "null"}</p> }
+
+
+                {/* <pre>{JSON.stringify(jsonText[0][item], null, 2)}</pre> */}
+              
+              
             </div>
           ))}
 
