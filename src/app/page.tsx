ce36a6b3f5adf50d3d7cfb106f0dd57f5e6af695
Paulo Turino <paulo.turino01@gmail.com>
2023-10-24 15:00:28 -0300

    feat: Add some signs on tree view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,78 +16,109 @@ export default function Home() {
         <div className={styles.main}>
           <h2>{jsonFile?.name}</h2>
 
+          {/*              PRIMEIRO NÍVEL SEMPRE É UM ARRAY           */}
           {Object.keys(jsonText[0]).map((item, index) => (
             <div key={index}>
-              <p>{item}: </p>
+              <p>
+                {item}:{" "}
+                {typeof jsonText[0][item] === "object" &&
+                  Array.isArray(jsonText[0][item]) &&
+                  "["}
+              </p>
 
+              {/*            SEGUNDO NÍVEL É UM OBJETO ?                */}
               {typeof jsonText[0][item] === "object" &&
-              !Array.isArray(jsonText[0][item]) ? (
-                Object.keys(jsonText[0][item]).map((item2, index) => (
-                  <div key={index} className={styles.treeDiv}>
-                    <p>{item2}: </p>
-                    
-                    {typeof jsonText[0][item][item2] === "object" &&
-                      !Array.isArray(jsonText[0][item][item2]) ? (
-                        Object.keys(jsonText[0][item][item2]).map((item, index) => (
-                          <div key={index} className={styles.treeDiv}>
-                            <p>{item}: </p>
-                          </div>
-                        ))
+              !Array.isArray(jsonText[0][item])
+                ? Object.keys(jsonText[0][item]).map((item2, index) => (
+                    <div key={index} className={styles.treeDiv}>
+                      <p>{item2}: </p>
 
-                      ) : typeof jsonText[0][item][item2] === "object" &&
+                      {/*     SEGUNDO NIVEL OBJETO E TERCEIRO NÍVEL OBJETO ?                */}
+                      {typeof jsonText[0][item][item2] === "object" &&
+                      !Array.isArray(jsonText[0][item][item2]) ? (
+                        Object.keys(jsonText[0][item][item2]).map(
+                          (item, index) => (
+                            <div key={index} className={styles.treeDiv}>
+                              <p>{item}:</p>
+                            </div>
+                          )
+                        )
+                      ) : //   SEGUNDO NIVEL OBJETO E TERCEIRO NÍVEL UM ARRAY ?
+                      typeof jsonText[0][item][item2] === "object" &&
                         Array.isArray(jsonText[0][item][item2]) ? (
-                        jsonText[0][item][item2].map((item: any, index: number) => (
-                          <div key={index} className={styles.treeDiv}>
-                            <p>{item}</p>
-                          </div>
-                        ))
-                        
+                        jsonText[0][item][item2].map(
+                          (item: any, index: number) => (
+                            <div key={index} className={styles.treeDiv}>
+                              <p>
+                                {item}: {"["}
+                              </p>
+                            </div>
+                          )
+                        )
                       ) : (
+                        //      SEGUNDO NIVEL OBJETO E TERCEIRO NÍVEL NUMBER OU STRING ?
                         <p className={styles.treeDiv}>
-                          {jsonText[0][item][item2] ? jsonText[0][item][item2] : "null"}
+                          {typeof jsonText[0][item][item2] === "number"
+                            ? jsonText[0][item][item2]
+                            : typeof jsonText[0][item][item2] === "string"
+                            ? `"${jsonText[0][item][item2]}"`
+                            : "null"}
                         </p>
                       )}
-                    
-                  </div>
-                ))
+                    </div>
+                  ))
+                : //             SEGUNDO NÍVEL É UM ARRAY ?
+                typeof jsonText[0][item] === "object" &&
+                  Array.isArray(jsonText[0][item])
+                ? jsonText[0][item].map((item2: any, index: number) => (
+                    <div key={index} className={styles.treeDiv}>
+                      <p>{item2}</p>
 
-              ) : typeof jsonText[0][item] === "object" &&
-                Array.isArray(jsonText[0][item]) ? (
-                jsonText[0][item].map((item2: any, index: number) => (
-                  <div key={index} className={styles.treeDiv}>
-                    <p>{item2}</p>
-
-                    {typeof jsonText[0][item][item2] === "object" &&
-                      !Array.isArray(jsonText[0][item][item2]) ? (
-                        Object.keys(jsonText[0][item][item2]).map((item, index) => (
-                          <div key={index} className={styles.treeDiv}>
-                            <p>{item}: </p>
-                          </div>
-                        ))
-
-                      ) : typeof jsonText[0][item][item2] === "object" &&
-                        Array.isArray(jsonText[0][item][item2]) ? (
-                        jsonText[0][item][item2].map((item: any, index: number) => (
-                          <div key={index} className={styles.treeDiv}>
-                            <p>{item}</p>
-                          </div>
-                        ))
-                        
-                      ) : jsonText[0][item][item2] && (
-                        <p className={styles.treeDiv}>
-                          {jsonText[0][item][item2] ? jsonText[0][item][item2] : "null"}
-                        </p>
-                      )}
-                  </div>
-                ))
-                
-              ) :jsonText[0][item] && (
-                <p className={styles.treeDiv}>
-                  {jsonText[0][item] ? jsonText[0][item] : "null"}
-                </p>
-              )}
-
-              {/* <pre>{JSON.stringify(jsonText[0][item], null, 2)}</pre> */}
+                      {/*                             SEGUNDO NIVEL ARRAY E TERCEIRO NÍVEL UM OBJETO ?                            */}
+                      {typeof jsonText[0][item][item2] === "object" &&
+                      !Array.isArray(jsonText[0][item][item2])
+                        ? Object.keys(jsonText[0][item][item2]).map(
+                            (item, index) => (
+                              <div key={index} className={styles.treeDiv}>
+                                <p>{item}: </p>
+                              </div>
+                            )
+                          )
+                        : //                              SEGUNDO NIVEL ARRAY E TERCEIRO NÍVEL ARRAY ?
+                        typeof jsonText[0][item][item2] === "object" &&
+                          Array.isArray(jsonText[0][item][item2])
+                        ? jsonText[0][item][item2].map(
+                            (item: any, index: number) => (
+                              <div key={index} className={styles.treeDiv}>
+                                <p>{item}:</p>
+                              </div>
+                            )
+                          )
+                        : //                             SEGUNDO NIVEL ARRAY E TERCEIRO NÍVEL NUMBER OU STRING ?
+                          jsonText[0][item][item2] && (
+                            <p className={styles.treeDiv}>
+                              {typeof jsonText[0][item][item2] === "number"
+                                ? jsonText[0][item][item2]
+                                : typeof jsonText[0][item][item2] === "string"
+                                ? `"${jsonText[0][item][item2]}"`
+                                : "null"}
+                            </p>
+                          )}
+                    </div>
+                  ))
+                : //                              SEGUNDO NÍVEL É NUMBER OU STRING ?
+                  jsonText[0][item] && (
+                    <p className={styles.treeDiv}>
+                      {typeof jsonText[0][item] === "number"
+                        ? jsonText[0][item]
+                        : typeof jsonText[0][item] === "string"
+                        ? `"${jsonText[0][item]}"`
+                        : "null"}
+                    </p>
+                  )}
+              {typeof jsonText[0][item] === "object" &&
+                Array.isArray(jsonText[0][item]) &&
+                "]"}
             </div>
           ))}
         </div>
