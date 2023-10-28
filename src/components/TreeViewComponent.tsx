6cdbbb27b3cd41bfe070eef530891873be9e68bb
Paulo Turino <paulo.turino01@gmail.com>
2023-10-28 15:26:33 -0300

    style: Add {} or [] at objects and arrays

--- src/components/TreeViewComponent.tsx
+++ src/components/TreeViewComponent.tsx
@@ -11,17 +11,19 @@ export default function TreeViewComponent({}) {
     return(
         <div className={styles.treeMain}>
             {jsonText && Object.keys(jsonText[0]).map((item, index) => (
+              <>
             <div key={index}>
-              <p>{item}: </p>
-              {typeof jsonText[0][item] === "object" && !Array.isArray(jsonText[0][item]) ?
+              <p>{`${item}: ${typeof jsonText[0][item] === "object" && !Array.isArray(jsonText[0][item]) ? "{" : typeof jsonText[0][item] === "object" && Array.isArray(jsonText[0][item]) ? "[" : ""}`}</p>
+              {typeof jsonText[0][item] === "object" && !Array.isArray(jsonText[0][item]) ? 
                 Object.keys(jsonText[0][item]).map((subItem, subIndex) => (
-                  <div key={subIndex} className={styles.treeDiv}>
+                  <div key={subIndex} className={styles.treeDiv}> 
                     <p>{subItem}: </p>
 
                     <TreeViewComponent2 json={jsonText[0][item][subItem]} />
-                  </div>
-                )) : typeof jsonText[0][item] === "object" && Array.isArray(jsonText[0][item]) ?
-                jsonText[0][item].map((subItem: any, subIndex: number) => (
+                    
+                    </div>
+                )) : typeof jsonText[0][item] === "object" && Array.isArray(jsonText[0][item]) ? 
+                 jsonText[0][item].map((subItem: any, subIndex: number) => (
                   <div key={subIndex} className={styles.treeDiv}>
                     <p>{`"${subItem}"`}</p>
 
@@ -29,6 +31,8 @@ export default function TreeViewComponent({}) {
                   </div>
               )): <p className={styles.treeDiv}>{ jsonText[0][item] === "" ? "null" : typeof jsonText[0][item] === "number" ? jsonText[0][item] : typeof jsonText[0][item] === "string" && `"${jsonText[0][item]}"`}</p> }              
             </div>
+            <p>{typeof jsonText[0][item] === "object" && !Array.isArray(jsonText[0][item]) ? "}" : typeof jsonText[0][item] === "object" && Array.isArray(jsonText[0][item]) ? "]" : ""}</p>
+            </>
           ))}
         </div>
     )
--- src/components/TreeViewComponent2.tsx
+++ src/components/TreeViewComponent2.tsx
@@ -14,18 +14,18 @@ export default function TreeViewComponent2({ json }: TreeViewProps) {
             <div key={index}>
               {typeof json[item] === "object" && !Array.isArray(json[item]) ? (
                 Object.keys(json).map((subItem, index) => (
-                  <div key={index} className={styles.treeDiv2}>
+                  <div key={index} className={styles.treeDiv}>
                     <p>{subItem}: </p>
                   </div>
                 ))
               ) : typeof json === "object" && Array.isArray(json[item]) ? (
                 json.map((subItem: any, index: number) => (
-                  <div key={index} className={styles.treeDiv2}>
+                  <div key={index} className={styles.treeDiv}>
                     <p>{`"${subItem}"`}</p>
                   </div>
                 ))
               ) : typeof json[0] === "string" && (
-                <p className={styles.treeDiv2}>{json ? `"${json}"` : "null"}</p>
+                <p className={styles.treeDiv}>{json ? `"${json}"` : "null"}</p>
               )}
             </div>
           ))
@@ -34,21 +34,21 @@ export default function TreeViewComponent2({ json }: TreeViewProps) {
             <div key={index}>
               {typeof json[item] === "object" && !Array.isArray(json[item]) ? (
                 Object.keys(json[item]).map((subItem, index) => (
-                  <div key={index} className={styles.treeDiv2}>
+                  <div key={index} className={styles.treeDiv}>
                     <p>{subItem}: </p>
                   </div>
                 ))
               ) : Array.isArray(json) ? (
                 json.map((subItem: any, index: number) => (
-                  <div key={index} className={styles.treeDiv2}>
+                  <div key={index} className={styles.treeDiv}>
                     <p>{subItem}: </p>
                   </div>
                 ))
               ) : (
-                <p className={styles.treeDiv2}>{ json[item] === "" ? "null" : typeof json[item] === "number" ? json[item] : typeof json === "string" && `"${json[item]}"`}</p>
+                <p className={styles.treeDiv}>{ json[item] === "" ? "null" : typeof json[item] === "number" ? json[item] : typeof json === "string" && `"${json[item]}"`}</p>
               )}
             </div>
-          )): <p className={styles.treeDiv2}>{ json === "" ? "null" : typeof json === "number" ? json : typeof json === "string" && `"${json}"`}</p> }
+          )): <p className={styles.treeDiv}>{ json === "" ? "null" : typeof json === "number" ? json : typeof json === "string" && `"${json}"`}</p> }
     </div>
   );
 }
